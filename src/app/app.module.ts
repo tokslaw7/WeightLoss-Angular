065075401a15ab08fdc138a5214c60c255520bbc
tokslaw <tokslaw@chiefsoft.com>
2023-08-06 12:05:36 -0400

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
