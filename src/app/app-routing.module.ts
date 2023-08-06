f828774b49cadef717c45377e69034df78c4a4a7
tokslaw <tokslaw@chiefsoft.com>
2023-08-06 13:28:24 -0400

    register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
 
 
 
 const routes: Routes = [
-  { path:'',
-    redirectTo: '/home',
-    pathMatch: 'full',
-    component:HomeComponent
-  },
-  { path:'home',
-    component:HomeComponent
-  }
+  { path:'', redirectTo: '/home', pathMatch: 'full'},
+  { path:'home', component:HomeComponent },
+  { path:'register', component:RegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
-    //AppRoutingModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
