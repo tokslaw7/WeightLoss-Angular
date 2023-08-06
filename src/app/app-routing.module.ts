364f435be92b02ef3e5dba7ac4c22cfe732521ed
tokslaw <tokslaw@chiefsoft.com>
2023-08-06 19:36:36 -0400

    employees comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
+import { EmployeesComponent } from "./employees/employees.component";
 
 
 
 const routes: Routes = [
   { path:'', redirectTo: '/home', pathMatch: 'full'},
   { path:'home', component:HomeComponent },
-  { path:'register', component:RegisterComponent }
+  { path:'register', component:RegisterComponent },
+  { path:'employees', component:EmployeesComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import {AppRoutingModule} from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
