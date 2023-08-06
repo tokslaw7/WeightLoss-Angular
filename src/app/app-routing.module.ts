08d0eeaddcaa010d8aed0748c84d0a8a7b1a9490
tokslaw <tokslaw@chiefsoft.com>
2023-08-06 12:39:54 -0400

    json server added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+
+
+
+const routes: Routes = [
+  { path:'',
+    redirectTo: '/home',
+    pathMatch: 'full',
+    component:HomeComponent
+  },
+  { path:'home',
+    component:HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
